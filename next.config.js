f245e2568e319abffa837ada1e943f37d8cdef08
sidneyramos <sidney.ramos95@gmail.com>
2020-01-29 00:36:45 +1100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,15 @@
-const withFonts = require('next-fonts');
+const withFonts = require("next-fonts");
 
-const withSass = require('@zeit/next-sass');
-const withCss = require('@zeit/next-css');
+const withSass = require("@zeit/next-sass");
+const withCss = require("@zeit/next-css");
 
-module.exports =
-withCss(
+module.exports = withCss(
   withSass(
-    withFonts(
-      {
-        webpack: config => {
-          // Fixes npm packages that depend on `fs` module
-          config.node = {
-            fs: 'empty'
-          }
-
-          return config
-        },
-        devIndicators: {
-          autoPrerender: false,
-        },
-        target: "serverless",
-      }
-    )
+    withFonts({
+      devIndicators: {
+        autoPrerender: false
+      },
+      target: "serverless"
+    })
   )
-)
+);
